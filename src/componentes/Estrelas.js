e86a0a4fc4c9f1bb9fe7204b7db98a34931fa13c
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-12-16 23:22:46 -0300

    Add stars part 2

--- src/componentes/Estrelas.js
+++ src/componentes/Estrelas.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
 import estrela from '../assets/estrela.png'
 import estrelaCinza from '../assets/estrelaCinza.png'
 export default function Estrelas({
@@ -7,10 +7,34 @@ export default function Estrelas({
     editavel = false,
     grande = false,
 }) {
+    const  [quantidade, setQuantidade ] = useState(quantidadeAntiga)
     const estilos = estilosFuncao(grande)
+
+    const getImage = (index) => {
+        if(index < quantidade) {
+            return estrela
+        }
+        return estrelaCinza
+    }
+
+    const RenderEstrelas = () => {
+        const listaEstrelas = []
+        for(let i = 0; i < 5; i++) {
+            listaEstrelas.push(
+                <TouchableOpacity 
+                    key={i}
+                    onPress={() => setQuantidade(i + 1)}
+                    disabled={!editavel}
+                >  
+                    <Image style={estilos.estrela} source={ getImage(i) } />
+                </TouchableOpacity>
+            )
+        }
+
+        return listaEstrelas
+    }
     return  <View style={estilos.estrelas}>
-                <Image style={estilos.estrela} source={ estrela } />
-                <Image style={estilos.estrela} source={ estrela } />
+                <RenderEstrelas />
             </View>
         
 }
--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,19 +1,27 @@
-import React from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import Estrelas from "../../../componentes/Estrelas";
 
 export default function Produtor({ nome, imagem, distancia, estrelas })  {
-    return  <View style={estilos.cartao}>
+    const [selecionado, setSelecionado] = useState(false);
+    return  <TouchableOpacity 
+                style={estilos.cartao}
+                onPress={() => setSelecionado(!selecionado)}
+            >
                 <Image style={estilos.imagem} source={ imagem } accessibilityLabel={ nome } />
                 <View style={estilos.informacoes}>
                     <View>
                         <Text style={estilos.nome}> { nome } </Text>
-                        <Estrelas quantidade={ estrelas } />
+                        <Estrelas   
+                            quantidade={ estrelas } 
+                            editavel={selecionado}
+                            grande={selecionado}
+                        />
                     </View>
                     <Text style={estilos.distancia}> { distancia } </Text>
                 </View>
                 
-            </View>
+            </TouchableOpacity>
 }
 
 const estilos = StyleSheet.create({
