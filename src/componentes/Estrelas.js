aefb58424584a12a3c465cc6b68ae305da6d741a
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2022-12-17 23:09:30 -0300

    refatorando o componente de estrelas

--- src/componentes/Estrelas.js
+++ src/componentes/Estrelas.js
@@ -1,33 +1,23 @@
 import React, { useState } from 'react'
-import { Image, StyleSheet, TouchableOpacity, View } from 'react-native'
-import estrela from '../assets/estrela.png'
-import estrelaCinza from '../assets/estrelaCinza.png'
+import { StyleSheet, View } from 'react-native'
+import Estrela from './Estrela'
 export default function Estrelas({
     quantidade: quantidadeAntiga,
     editavel = false,
     grande = false,
 }) {
     const  [quantidade, setQuantidade ] = useState(quantidadeAntiga)
-    const estilos = estilosFuncao(grande)
-
-    const getImage = (index) => {
-        if(index < quantidade) {
-            return estrela
-        }
-        return estrelaCinza
-    }
-
     const RenderEstrelas = () => {
         const listaEstrelas = []
         for(let i = 0; i < 5; i++) {
             listaEstrelas.push(
-                <TouchableOpacity 
+                <Estrela 
                     key={i}
                     onPress={() => setQuantidade(i + 1)}
-                    disabled={!editavel}
-                >  
-                    <Image style={estilos.estrela} source={ getImage(i) } />
-                </TouchableOpacity>
+                    desabilitada={!editavel}
+                    preenchida={i < quantidade}
+                    grande={grande}
+                />
             )
         }
 
@@ -39,13 +29,8 @@ export default function Estrelas({
         
 }
 
-const estilosFuncao = (grande) => StyleSheet.create({
+const estilos = StyleSheet.create({
     estrelas: {
         flexDirection: "row"
     },
-    estrela: {
-        width: grande ? 36 : 12,
-        height: grande ? 36 : 12,
-        marginRight: 2
-    }
 })
